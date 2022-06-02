691ab1d8d0691f8ad8384ef8d177be81df75487c
Brian <brtsuper07@gmail.com>
2022-06-02 18:50:42 +0900

    Fixed some bugs within cmd_validator.ps1

--- cmd_validator.ps1
+++ cmd_validator.ps1
@@ -1,81 +1,72 @@
-﻿try {
-    $ciEnc = [console]::InputEncoding
-    $coEnc = [console]::OutputEncoding
-    $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding        
-    $Global:promptPrimary = &oh-my-posh print -p primary 
-    $Global:promptSecondary = &oh-my-posh print -p secondary
-    $Global:promptDebug = &oh-my-posh print -p debug
-}
-finally {
-    [console]::InputEncoding = $ciEnc
-    [console]::OutputEncoding = $coEnc
-}
- 
-Set-PSReadLineKeyHandler -Key " " `
-    -BriefDescription ValidatePrograms `
-    -LongDescription "Validate typed program's existance in path variable" `
-    -ScriptBlock {
-    param($key, $arg)
+﻿$global:lastRender = Get-Date  
+$printableChars = [char[]] (0x20..0x7e + 0xa0..0xff)
+$printableChars + "Tab" | ForEach-Object {
+    Set-PSReadLineKeyHandler -Key $_ `
+        -BriefDescription ValidatePrograms `
+        -LongDescription "Validate typed program's existance in path variable" `
+        -ScriptBlock {
+        param($key, $arg)   
 
-    try {     
-        
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)")            
-        $ast = $null; $tokens = $null ; $errors = $null; $cursor = $null
-        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)  
-        $cursorPosY = $host.UI.RawUI.CursorPosition.Y
-        $token = $tokens[0]
-        $scanY = $cursorPosY
-        $completeBufStr = New-Object System.Text.StringBuilder
-        $foundIndex = -1
-        $offset = 0
-        while ($foundIndex -eq -1) {
-            $rec = New-Object System.Management.Automation.Host.Rectangle(0, $scanY, $host.ui.RawUI.BufferSize.Width, $scanY)
-            $buf = $host.UI.RawUI.GetBufferContents($rec)     
-            $lineInBuf = New-Object System.Text.StringBuilder
-            $buf  | ForEach-Object { [void] $lineInBuf.Append($_.Character) }
-            $line = $lineInBuf.ToString() 
-          
-            if ($line.StartsWith($Global:promptPrimary)) {
-                $offset=  $Global:promptPrimary.Length
-                $line = $line.Remove(0,$offset)               
-            }
-            elseif ($line.StartsWith($Global:promptSecondary)) {
-                $offset=  $Global:promptSecondary.Length
-                $line = $line.Remove(0, $offset)
-            }
-            elseif ($line.StartsWith($Global:promptDebug)) {
-                $offset=  $Global:promptDebug.Length
-                $line = $line.Remove(0,$offset)
-            }                  
-            $completeBufStr.Insert(0, $line.Trim())    
-            $foundIndex = $completeBufStr.ToString().IndexOf($token.Text)
-            if ($foundIndex -gt -1) {
-                break
-            }
-            else {
-                $scanY--
-            }
+        if ( $key.Key -ne [System.ConsoleKey]::Tab) {
+            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($key.KeyChar)
         }
-        $foundIndex +=$offset
+        else {
+            [Microsoft.PowerShell.PSConsoleReadLine]::TabCompleteNext($key)
+        } 
 
-        $tokenLength = ($token.Extent.EndOffset - $token.Extent.StartOffset)
-        $finalRec = New-Object System.Management.Automation.Host.Rectangle($foundIndex, $scanY, ($foundIndex + $tokenLength), $scanY)            
-        $finalBuf = $host.UI.RawUI.GetBufferContents($finalRec)
-        $t = $finalBuf.GetValue(0, 0) 
-        if ($t.ForegroundColor -eq "Red" -or $t.ForegroundColor -eq "Green") {
+        if (((get-date) - $global:lastRender).TotalMilliseconds -le 50) {
             return
         }
+
+        $ast = $null; $tokens = $null ; $errors = $null; $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)  
+        $token = $tokens[0]
+        if ([string]::IsNullOrEmpty($token.Text.Trim())) {
+            return
+        }  
+
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition(0)
+        $cursorPosX = $host.UI.RawUI.CursorPosition.X
+        $cursorPosY = $host.UI.RawUI.CursorPosition.Y
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor)
+
+        $tokenLength = ($token.Extent.EndOffset - $token.Extent.StartOffset)
+ 
         $color = "Red"
-        if ((Get-Command $token -ErrorAction SilentlyContinue) -or (Get-Command "$token.exe" -ErrorAction SilentlyContinue)) {
+        if ((Get-Command $token -ErrorAction SilentlyContinue)) {
             $color = "Green"
-        }    
-        for ($xPosition = 0; $xPosition -lt $tokenLength; $xPosition++) {
-            $bufferItem = $finalBuf.GetValue(0, $xPosition)      
-            $bufferItem.ForegroundColor = $color          
-            $finalBuf.SetValue($bufferItem, 0, $xPosition)         
+        } 
+
+        $sX = $cursorPosX
+        $Y = $cursorPosY
+        $eX = ($cursorPosX + $tokenLength)
+        $nextLine = $false
+
+        $painted = 0
+        $bufSize = $host.UI.RawUI.BufferSize.Width
+        while ($painted -ne $tokenLength) {            
+            $scanXEnd = $eX
+            if ($eX -gt $bufSize) {
+                $scanXEnd = $bufSize
+                $eX = $eX - $bufSize
+                $nextLine = $true
+            } 
+            $finalRec = New-Object System.Management.Automation.Host.Rectangle($sX, $Y, $scanXEnd, $Y)            
+            $finalBuf = $host.UI.RawUI.GetBufferContents($finalRec)
+            for ($xPosition = 0; $xPosition -lt ($scanXEnd - $sX); $xPosition++) {
+                $bufferItem = $finalBuf.GetValue(0, $xPosition)      
+                $bufferItem.ForegroundColor = $color          
+                $finalBuf.SetValue($bufferItem, 0, $xPosition)   
+                $painted++      
+            }
+            $coords = New-Object System.Management.Automation.Host.Coordinates $sX , $Y
+            $host.ui.RawUI.SetBufferContents($coords, $finalBuf)
+            if ($nextLine) {
+                $sX=0
+                $Y++
+                $nextLine = $false
+            }
         }
-        $coords = New-Object System.Management.Automation.Host.Coordinates $foundIndex , ($scanY)
-        $host.ui.RawUI.SetBufferContents($coords, $finalBuf)
+        $global:lastRender = get-date
     }
-    catch {  }    
 }
